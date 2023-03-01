88ebb5d1b8a4546728294c5db8cc9d9e186ce5b9
Nitish Mahawar <nitishmahawar112@gmail.com>
2023-03-01 20:25:28 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [prompt, setPrompt] = useState("");
   const [image, setImage] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_API_KEY,
   });
@@ -12,14 +13,18 @@ const App = () => {
 
   const getImage = async () => {
     setLoading(true);
-    const response = await openai.createImage({
-      prompt: prompt,
-      n: 1,
-      size: "1024x1024",
-    });
+    try {
+      const response = await openai.createImage({
+        prompt: prompt,
+        n: 1,
+        size: "1024x1024",
+      });
+      setImage(response.data.data[0].url);
+    } catch (err) {
+      setError(err.message);
+      console.log(err.message);
+    }
     setLoading(false);
-    console.log(response.data.data[0].url);
-    setImage(response.data.data[0].url);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,26 +35,31 @@ const App = () => {
   };
   return (
     <div className="px-4 py-8 space-y-4">
-      <h1 className="text-6xl font-medium font-serif text-center">Dall E </h1>
-
+      <h1 className="text-6xl font-medium font-serif text-center flex items-center justify-center">
+        Dall<span className="text-4xl">•</span>E
+      </h1>
       <form
-        className="max-w-lg mx-auto overflow-hidden border text-lg md:text-xl rounded-lg flex items-center border-black/50"
+        className="max-w-lg mx-auto overflow-hidden border text-lg rounded-xl flex items-center border-black/50"
         onSubmit={handleSubmit}
       >
         <input
           type="text"
-          className="px-4 py-3 w-full outline-none"
+          className="px-4 py-2 w-full outline-none"
           placeholder="An astronaut riding horse!"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
         />
-        <button className="py-2 px-4 bg-teal-500 text-white font-semibold rounded-md m-1">
+        <button className="py-2 px-4 text-white font-semibold bg-black">
           Generate
         </button>
       </form>
       <div className="max-w-sm rounded-xl overflow-hidden mx-auto aspect-square">
         {loading ? (
-          <div className="h-full w-full bg-neutral-300 animate-pulse"></div>
+          <div className="h-full w-full bg-neutral-300 animate-pulse flex items-center justify-center text-xl font-semibold">
+            Generating...
+          </div>
+        ) : error ? (
+          <div>{error}</div>
         ) : (
           <img src={image} alt="" />
         )}
